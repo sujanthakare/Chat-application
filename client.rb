6f3e4e5c7acdb7fb0ccd27f92c23d450a7e32241
sujanthakare <sujandt@gmail.com>
2014-07-07 23:47:22 +0530

    file added

---
+++ client.rb
@@ -0,0 +1,38 @@
+require "socket"
+
+class client
+	def initialize(server)
+		@server = server
+		send
+		listen
+		@request
+		@response
+		@request.join
+		@response.join
+	end
+
+	def send()
+		@request = Thread.new  do
+			loop { 
+
+					msg = gets.chomp
+					@server.puts( msg )
+			 }
+		end
+		
+	end
+	def listen
+		@response = Thread.new do
+			loop { 
+
+					msg  = @server.gets.chomp
+					puts "#{msg}"
+	
+			 }	
+		end
+	end
+end
+
+
+server = TCPSocket.open ARGV[0], ARGV[1]
+client.new( server )
---
+++ server.rb
@@ -0,0 +1,82 @@
+require "socket"
+
+class Server
+	def initialize(port,ip)
+		@server = TCPServer.open(ip,port)
+
+		@connections = Hash.new
+		@name = Hash.new
+		@clients = Hash.new
+
+
+		@connections[:name] = @name
+		@connections[:client] = @clients
+
+		run
+	end
+
+	def run
+		puts "you are in run"
+		puts "server ready to accept client connections"
+
+		loop { 
+				Thread.start(@server.accept) do |client|
+					client.puts "Enter Username"
+					username = client.gets.chomp
+					@connections[:client].each do |other_name, other_client|
+						if username == other_name || client == other_client
+							client.puts "username already exists"
+							client.puts "try connecting again with another username"
+							client.close		
+						end
+					end
+					@connections[:client][username] = client 
+					client.puts	 "welcome #{username} you are ready to chat"
+					client.puts  "type 'exit' to leave "
+					available_user(client)
+
+					broadcast_to_all(username,client)
+			end
+		 
+		 }
+	end
+
+	def available_user(client)
+		
+			client.puts "Users available"
+			@connections[:client].each do |other_name, other_client|
+			client.puts "#{other_name}"  						
+		end
+	end
+
+		
+	def broadcast_to_all(username,client)
+
+	
+		puts "you are in broadcast_to_all method"
+
+		loop {
+
+
+		msg = client.gets.chomp
+		if msg.to_s == "exit"
+		@connections[:client] = @connections.delete(client)
+			client.close
+			break
+		end
+      
+      	@connections[:client].each do |other_name, other_client|
+        
+        unless other_name == username  
+          puts "#{username.to_s}: #{msg}"	
+          other_client.puts "#{username.to_s}: #{msg}"
+        end
+    	
+    	end
+	}
+			
+	end
+
+end
+
+Server.new ARGV[0] , ARGV[1]
